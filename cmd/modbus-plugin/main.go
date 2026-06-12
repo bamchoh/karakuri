e07b1e8a337428fc29c3c1ba777a270771c4d02d
bamchoh <bamchoh@gmail.com>
2026-06-12 22:11:06 +0900

    refactor: extract common plugin bootstrap logic into pluginruntime

--- cmd/modbus-plugin/main.go
+++ cmd/modbus-plugin/main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net"
 	"os"
-	"os/signal"
-	"syscall"
-
-	"google.golang.org/grpc"
 
 	"modbus_simulator/cmd/modbus-plugin/server"
+	"modbus_simulator/internal/pluginruntime"
 )
 
 func main() {
@@ -19,33 +15,10 @@ func main() {
 	flag.Parse()
 
 	fmt.Fprintln(os.Stderr, "Modbus Plugin starting... protocol-type="+*protocolType)
-	// ランダムな空きポートで gRPC サーバーを起動
-	lis, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[ERROR] gRPC リスナー起動失敗: %v\n", err)
+
+	plugin := server.NewPluginServer(*protocolType)
+	if err := pluginruntime.Run(plugin); err != nil {
+		fmt.Fprintf(os.Stderr, "[ERROR] プラグインランタイムエラー: %v\n", err)
 		os.Exit(1)
 	}
-	port := lis.Addr().(*net.TCPAddr).Port
-
-	// gRPC サーバーを作成してサービスを登録
-	grpcServer := grpc.NewServer()
-	pluginSrv := server.NewPluginServer(*protocolType)
-	pluginSrv.Register(grpcServer)
-
-	// サーバーをバックグラウンドで起動
-	go func() {
-		if err := grpcServer.Serve(lis); err != nil {
-			fmt.Fprintf(os.Stderr, "[ERROR] gRPC サーバーエラー: %v\n", err)
-		}
-	}()
-
-	// ホストが読み取るポート番号を stdout に出力
-	fmt.Printf("GRPC_PORT=%d\n", port)
-
-	// SIGTERM/SIGINT でグレースフルシャットダウン
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
-	<-sigCh
-
-	grpcServer.GracefulStop()
 }
---
+++ internal/pluginruntime/runtime.go
@@ -0,0 +1,58 @@
+package pluginruntime
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+)
+
+type Plugin interface {
+	Register(*grpc.Server)
+}
+
+func Run(plugin Plugin) error {
+	lis, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return fmt.Errorf("listen: %w", err)
+	}
+
+	port := lis.Addr().(*net.TCPAddr).Port
+
+	grpcServer := grpc.NewServer()
+	plugin.Register(grpcServer)
+
+	serverErrCh := make(chan error, 1)
+
+	go func() {
+		err := grpcServer.Serve(lis)
+
+		// GracefulStop() 後の正常終了は無視
+		if err != nil && !errors.Is(err, grpc.ErrServerStopped) {
+			serverErrCh <- err
+		}
+		close(serverErrCh)
+	}()
+
+	fmt.Printf("GRPC_PORT=%d\n", port)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+	defer signal.Stop(sigCh)
+
+	select {
+	case <-sigCh:
+		grpcServer.GracefulStop()
+		return nil
+
+	case err := <-serverErrCh:
+		if err != nil {
+			return fmt.Errorf("grpc server: %w", err)
+		}
+		return nil
+	}
+}
